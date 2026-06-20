da39e4beec73bfed9312c7501b399fd37983b060
Dalton Wood <wood.dalton27@gmail.com>
2026-06-19 22:10:29 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/dev/daltonwood/netscan/NetscanApplication.java
@@ -0,0 +1,13 @@
+package dev.daltonwood.netscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetscanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/daltonwood/netscan/NetscanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.daltonwood.netscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
